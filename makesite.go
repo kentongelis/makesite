ed21fb5113a97ca61a039db6835526fe5e4e2e28
kentongelis <kenton.gelis@students.dominican.edu>
2026-09-07 15:31:28 -0700

    finished 1.1

--- makesite.go
+++ makesite.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"flag"
 	"strings"
+	"log"
 )
 
 type Page struct {
@@ -17,40 +18,58 @@ type Page struct {
 
 func main() {
 	// Read the command line and define a flag
-	filePtr := flag.String("file", "first-post.txt", "text file to render")
+	// filePtr := flag.String("file", "first-post.txt", "text file to render")
+	// flag.Parse()
+
+	// Read the command line and define a directory flag
+	dirPtr := flag.String("dir", ".", "directory to render files")
 	flag.Parse()
 
-	// Read file contents 
-	fileContents, err := ioutil.ReadFile(*filePtr)
+	// Get all files in given directory
+	files, err := ioutil.ReadDir(*dirPtr)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	// Create the page 
-	page := Page{
-		TextFilePath: *filePtr,
-		TextFileName: *filePtr,
-		HTMLPagePath: strings.TrimSuffix(*filePtr, ".txt") + ".html",
-		Content: string(fileContents),
-	}
+	// Loop over all files and skip over any file that doesn't end in .txt
+	for _, file := range files {
+		filename := file.Name()
+		if file.IsDir() || !strings.HasSuffix(filename, ".txt") {
+			continue
+		}
 
-	// Save a new template to memory by parsing the given template
-	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+		// Read file contents 
+		fileContents, err := ioutil.ReadFile(filename)
+		if err != nil {
+			panic(err)
+		}
 
-	// Print the result to stdout
-	if err := t.Execute(os.Stdout, page); err != nil {
-		panic(err)
-	}
-	
-	// Create a new file for the template the write to
-	newFile, err := os.Create(page.HTMLPagePath)
-	if err != nil {
-		panic(err)
-	}
-	defer newFile.Close()
+		// Create the page 
+		page := Page{
+			TextFilePath: filename,
+			TextFileName: filename,
+			HTMLPagePath: strings.TrimSuffix(filename, ".txt") + ".html",
+			Content: string(fileContents),
+		}
 
-	// Write the template to the new file
-	if err := t.Execute(newFile, page); err != nil {
-		panic(err)
+		// Save a new template to memory by parsing the given template
+		t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+		// Print the result to stdout
+		if err := t.Execute(os.Stdout, page); err != nil {
+			panic(err)
+		}
+		
+		// Create a new file for the template the write to
+		newFile, err := os.Create(page.HTMLPagePath)
+		if err != nil {
+			panic(err)
+		}
+
+		// Write the template to the new file
+		if err := t.Execute(newFile, page); err != nil {
+			panic(err)
+		}
+		newFile.Close()
 	}
 }

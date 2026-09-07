14c27cce13e21009314d3a4355714e56891425cb
kentongelis <kenton.gelis@students.dominican.edu>
2026-09-07 15:08:39 -0700

    mvp finished

--- makesite.go
+++ makesite.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"io/ioutil"
 	"os"
+	"flag"
+	"strings"
 )
 
 type Page struct {
@@ -14,31 +16,40 @@ type Page struct {
 }
 
 func main() {
+	// Read the command line and define a flag
+	filePtr := flag.String("file", "first-post.txt", "text file to render")
+	flag.Parse()
+
 	// Read file contents 
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+	fileContents, err := ioutil.ReadFile(*filePtr)
 	if err != nil {
 		panic(err)
 	}
 
+	// Create the page 
 	page := Page{
-		TextFilePath: "first-post.txt",
-		TextFileName: "first-post.txt",
-		HTMLPagePath: "first-post.html",
+		TextFilePath: *filePtr,
+		TextFileName: *filePtr,
+		HTMLPagePath: strings.TrimSuffix(*filePtr, ".txt") + ".html",
 		Content: string(fileContents),
 	}
 
+	// Save a new template to memory by parsing the given template
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
 
+	// Print the result to stdout
 	if err := t.Execute(os.Stdout, page); err != nil {
 		panic(err)
 	}
 	
-	newFile, err := os.Create("first-post.html")
+	// Create a new file for the template the write to
+	newFile, err := os.Create(page.HTMLPagePath)
 	if err != nil {
 		panic(err)
 	}
 	defer newFile.Close()
 
+	// Write the template to the new file
 	if err := t.Execute(newFile, page); err != nil {
 		panic(err)
 	}
